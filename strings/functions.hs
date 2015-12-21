fa7f0a4ec001602e00b5b627af451a96c04b6364
Achrine <achrine1@gmail.com>
2015-12-21 01:57:02 -0800

    first commit

---
+++ strings/functions.hs
@@ -0,0 +1,17 @@
+thirdLetter :: String -> Char
+thirdLetter x = x !! 2
+
+letterIndex :: Int -> Char
+letterIndex x = "Curry is awesome!" !! x
+
+excited x = x ++ "!"
+
+fourthPart y = y !! 4
+
+dropped z = drop 9 z
+
+rvrs = x ++ y ++ z 
+phrase = "curry is awesome"
+x = take 5 phrase
+y = drop 5 phrase take 3 phrase 
+z = drop 9 phrase
